d9f2534c59e683365d8e2b15103781ef2f6a628e
Zabai Armas Herrera <zbrmas@hotmail.com>
2023-04-25 19:56:17 +0100

    feat: add bulma helpers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
           href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
           rel="stylesheet"
         />
+        <link
+          crossOrigin="anonymous"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bulma-helpers/0.4.0/css/bulma-helpers.min.css"
+          integrity="sha512-U6ELnUi7oqVEjkLmFw5r5UR5LEtvpImS/jUykBKneVhD0lxZxfJZ3k3pe003ktrtNZYungd9u3Urp2X09wKwXg=="
+          rel="stylesheet"
+        />
       </Head>
 
       <body>
