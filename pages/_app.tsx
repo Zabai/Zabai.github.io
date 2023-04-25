9ad331288c2e9eb066b432bcbc6319ba7e3f6625
Zabai Armas Herrera <zbrmas@hotmail.com>
2023-04-25 12:55:26 +0100

    feat: add Bulma CSS and FontAwesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        crossOrigin="anonymous"
+        integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc"
+        src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
