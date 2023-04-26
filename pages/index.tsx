a57a163a39595bef9f212b9f2e10394e95e01953
Zabai Armas Herrera <zbrmas@hotmail.com>
2023-04-26 12:59:21 +0100

    feat: update index and add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import Contact from '@/features/Contact';
+import Cover from '@/features/Cover';
+import Credits from '@/features/Credits';
+import Experience from '@/features/Experience';
+import Navigator from '@/features/Navigator';
+import Resume from '@/features/Resume';
+import Studies from '@/features/Studies';
 import Head from 'next/head';
 
 export default function Home() {
@@ -7,9 +14,17 @@ export default function Home() {
         <title>Zabai Armas Herrera</title>
         <meta name="description" content="Zabai Armas Herrera portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
-      <main></main>
+      <main>
+        <Navigator />
+        <Cover />
+        <Experience />
+        <Studies />
+        <Resume />
+        <Contact />
+        <Credits />
+      </main>
     </>
   );
 }
