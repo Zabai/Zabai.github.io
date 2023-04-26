dde7b67632187bc1de0a888c7c60b9525a39f2bf
Zabai Armas Herrera <zbrmas@hotmail.com>
2023-04-26 14:39:40 +0100

    Updates

---
+++ _next/static/EpnfQmc4NpF1OPJrytZtZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d3569982ec5b9168.css","static/chunks/pages/index-b1d6dbda3c58361a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
